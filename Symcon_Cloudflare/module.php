9e0cdc18f9e6325f5b72ef5b9be3eab488b5e0fa
Timo Umstädter <timo@timo-u.de>
2018-08-24 12:00:32 +0200

    Add Autehntication Request

--- Symcon_Cloudflare/module.php
+++ Symcon_Cloudflare/module.php
@@ -49,6 +49,44 @@
 			
 		}
 		
+		public function UpdateRecord() {
 		
+		}
+		
+		public function Authenticate() {
+		
+		$curl = curl_init();
+																		
+		curl_setopt_array($curl, array(
+		CURLOPT_URL => "https://api.cloudflare.com/client/v4/zones",
+		CURLOPT_RETURNTRANSFER => true,
+		CURLOPT_ENCODING => "",
+		CURLOPT_MAXREDIRS => 10,
+		CURLOPT_TIMEOUT => 30,
+		CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+		CURLOPT_CUSTOMREQUEST => "GET",
+		CURLOPT_HTTPHEADER => array(
+		"cache-control: no-cache",
+		"x-auth-email: ".ReadPropertyInteger("MailAddress"),
+		"x-auth-key: ".ReadPropertyInteger("APIKey")
+		),
+		));
+
+		$response = curl_exec($curl);
+		$err = curl_error($curl);
+
+		curl_close($curl);
+		
+		if ($err) {
+		echo "cURL Error #:" . $err;
+		} else {
+		$obj = json_decode($response,true);
+		if( $obj["success"]!=1)
+		{
+		echo "GetZoneId Failed";
+		die;
+		}
+		
+		}
 	}
 ?>
